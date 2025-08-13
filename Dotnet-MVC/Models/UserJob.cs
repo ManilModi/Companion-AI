4807322ec7f831e1e16b2e6b99df2ea1ed4bcd42
DeepPrajapati1580 <166936398+DeepPrajapati1580@users.noreply.github.com>
2025-08-13 16:46:58 +0530

    added Repo interface

--- Dotnet-MVC/Models/UserJob.cs
+++ Dotnet-MVC/Models/UserJob.cs
@@ -6,10 +6,7 @@ namespace DotnetMVCApp.Models
     public class UserJob
     {
         public int UserId { get; set; }
-
-
         public User? User { get; set; }
-
         public int JobId { get; set; }
         public Job? Job { get; set; }
 
